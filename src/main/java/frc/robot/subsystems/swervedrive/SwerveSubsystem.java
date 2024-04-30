6c29edcac4d6455f966b459b005a286babc1317e
Loretto Drive Station <lorettolightning@gmail.com>
2024-04-30 17:50:46 -0500

    Meters

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -70,7 +70,8 @@ public class SwerveSubsystem extends SubsystemBase {
   /**
    * Maximum speed of the robot in meters per second, used to limit acceleration.
    */
-  public        double      maximumSpeed = Units.feetToMeters(19.0);
+  //public        double      maximumSpeed = Units.feetToMeters(19.0);
+  public        double      maximumSpeed = 5.7912;
 
   /**
    * Initialize {@link SwerveDrive} with the directory provided.
