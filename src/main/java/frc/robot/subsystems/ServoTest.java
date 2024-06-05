0ad581d4e9a9ad23097e4760a67fe75925d5cf00
Cameron Knoph <lorettolightning@gmail.com>
2024-06-05 11:51:00 -0500

    bounds

--- src/main/java/frc/robot/subsystems/ServoTest.java
+++ src/main/java/frc/robot/subsystems/ServoTest.java
@@ -9,37 +9,38 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ServoTest extends SubsystemBase{
     
-    public Servo servo;
+    public static Servo servofive;
 
     public ServoTest() {
-        servo = new Servo(5);
+        servofive = new Servo(5);
+        servofive.setBoundsMicroseconds(2500, 0, 1500, 0, 500);
     }
 
     public void addDashboardWidgets(ShuffleboardLayout layout) {
         layout.withProperties(Map.of("Number of columns", 2, "Number of rows", 4));
-        layout.addNumber("Servo Angle", servo::getAngle)                    .withPosition(0, 0);
-        layout.addNumber("Servo Channel", servo::getChannel)                .withPosition(0, 1);
-        layout.addNumber("Servo Position", servo::getPosition)              .withPosition(0, 2);
-        layout.addNumber("Servo Speed", servo::getSpeed)                    .withPosition(0, 3);
-        layout.addNumber("Servo get", servo::get)                           .withPosition(1, 0);
-        layout.addNumber("Servo Pulse", servo::getPulseTimeMicroseconds)    .withPosition(1, 1);
-        layout.addNumber("Servo Handle", servo::getHandle)                  .withPosition(1, 2);
+        layout.addNumber("Servo Angle", servofive::getAngle)                    .withPosition(0, 0);
+        layout.addNumber("Servo Channel", servofive::getChannel)                .withPosition(0, 1);
+        layout.addNumber("Servo Position", servofive::getPosition)              .withPosition(0, 2);
+        layout.addNumber("Servo Speed", servofive::getSpeed)                    .withPosition(0, 3);
+        layout.addNumber("Servo get", servofive::get)                           .withPosition(1, 0);
+        layout.addNumber("Servo Pulse", servofive::getPulseTimeMicroseconds)    .withPosition(1, 1);
+        layout.addNumber("Servo Handle", servofive::getHandle)                  .withPosition(1, 2);
     }
 
     public void setServoPosition(double position) {
-        servo.setPosition(position);
+        servofive.setPosition(position);
     }
 
     public void setServoAngle(double angle) {
-        servo.setAngle(angle);
+        servofive.setAngle(angle);
     }
 
     public void setServoSet(double value) {
-        servo.set(value);
+        servofive.set(value);
     }
 
     public void setServoSpeed(double speed) {
-        servo.setSpeed(speed);
+        servofive.setSpeed(speed);
     }
 
     public Command setServoSetCommand(double value) {
